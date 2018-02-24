3ce008cfde9e2162b170c1f2534531cada6e0d36
Nezmar23 <Nezmar23@seznam.cz>
2018-02-24 03:43:12 +0100

    pohyb po obrazovce

---
+++ screen1.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <curses.h>
+
+int main ()
+{
+	initscr();
+	move(5,15);
+	printw("%s","Nazdar");
+
+
+	move(25,25);
+        printw("%s","AHOJ");
+	refresh();
+	sleep(10);
+	endwin();
+        exit(EXIT_SUCCESS);
+}
+
+
